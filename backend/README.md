8a5b1ea40f9db682ff275b112cb5d1f96e0e6c1a
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-05-27 17:06:36 -0400

    api route

--- backend/README.md
+++ backend/README.md
@@ -37,7 +37,7 @@ From within the `./src` directory first ensure you are working using your create
 Each time you open a new terminal session, run:
 
 ```bash
-export FLASK_APP=api.py;
+set FLASK_APP=api.py;
 ```
 
 To run the server, execute:
@@ -52,8 +52,8 @@ The `--reload` flag will detect file changes and restart the server automaticall
 
 ### Setup Auth0
 
-1. Create a new Auth0 Account
-2. Select a unique tenant domain
+1. Create a new Auth0 Account : github  
+2. Select a unique tenant domain : coffeeshop-jolisa.auth0.com
 3. Create a new, single page web application
 4. Create a new API
     - in API Settings:
@@ -72,6 +72,8 @@ The `--reload` flag will detect file changes and restart the server automaticall
 7. Test your endpoints with [Postman](https://getpostman.com). 
     - Register 2 users - assign the Barista role to one and Manager role to the other.
     - Sign into each account and make note of the JWT.
+        + barista : eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im45b1UxYk5zQUViOXNUZF9XdnlzYiJ9.eyJpc3MiOiJodHRwczovL2NvZmZlZXNob3Atam9saXNhLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWNlOTAyNDBmZDcyMjBjNjVkZTZjYjQiLCJhdWQiOiJjb2ZmZWVzaG9wIiwiaWF0IjoxNTkwNjA3NDU4LCJleHAiOjE1OTA2MTQ2NTgsImF6cCI6IkZrR2txT1Y1VUxPZXMyYmtST1Z3MVY2dWhaZ0p3WWd4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJnZXQiXX0.3rsLkU1kwa5PS9owJtQdweeHRrh2A_I4jXz3_56sM0U7UHA7gJAZzohiXgyPPuvlXPIeWPNQ9MQ2XrfBK7c0bWhS7BC1Joatfn5BK_QvJjHZr9aN5DLsLVlIdYc3IO1bMffyerCxc8V-EwM_ghXUQ-FWcBJY1D31dgZamuuFtf8ivOTpuRRPEkDYi1Lkrqy152zUj0lQnKBHcZvHT4-fvMHs7pdsY_oa4LTzPS3g1I5LSxwEBTihEN3c8z9-Q70Mj_0iyMPo1VfqF9X0kCHEQc8paW1RUxzQXkBhR7rShSqzwOkLdE1LJjuPZIsSyso7uLjqzYQUAvyvZYRN_zut6g
+        + manager : eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im45b1UxYk5zQUViOXNUZF9XdnlzYiJ9.eyJpc3MiOiJodHRwczovL2NvZmZlZXNob3Atam9saXNhLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWNlOTA5MDBmZDcyMjBjNjVkZTZlNzciLCJhdWQiOiJjb2ZmZWVzaG9wIiwiaWF0IjoxNTkwNjA3NjQ2LCJleHAiOjE1OTA2MTQ4NDYsImF6cCI6IkZrR2txT1Y1VUxPZXMyYmtST1Z3MVY2dWhaZ0p3WWd4Iiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGUiLCJnZXQiLCJwYXRjaCIsInBvc3QiXX0.De1mr0VO-MvkxVr4ECycKUaNxhaqNP2h85mV76ldOssVpI7dlzK6JXcVwF8XgMFR165NhNHESUFiImPcKqgINXWIcfvfZo_KSrABtnmFhegbHLy3bi8xoFLqfDJJTo_nbt-Trpb6L9EHAYi-0B9ny1q3q8sLoeE0x-8TlhbHwmfdhGNmzI27gtay4eK98pmcWxwgOJOpk-kLtxE7BwLCqfhlLtNJrqKA73WqCRcotcSQFA0YR7HgH-T5IGwuG6XHzY13-0RZ__b4L5Ed0wNfEqzpAfi0rxeRDSmEwvVTf_R9CTJS1ROPFY4yh2EDhjR4JrhIcO9V3l1kX9pjz4UOFA
     - Import the postman collection `./starter_code/backend/udacity-fsnd-udaspicelatte.postman_collection.json`
     - Right-clicking the collection folder for barista and manager, navigate to the authorization tab, and including the JWT in the token field (you should have noted these JWTs).
     - Run the collection and correct any errors.
--- backend/src/api.py
+++ backend/src/api.py
@@ -16,31 +16,43 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
-@TODO implement endpoint
+@TODO DONE implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route("/drinks")
+def get_drinks():
+    datas = Drink.query.all()
+    drinks = [data.short() for data in datas]
+    return jsonify({'success': True,
+                    "drinks": drinks
+                    })
 
 '''
-@TODO implement endpoint
+@TODO DONE implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route("/drinks-detail")
+def get_drinks_detail():
+    datas = Drink.query.all()
+    drinks = [data.long() for data in datas]
+    return jsonify({'success': True,
+                    "drinks": drinks
+                    })
 
 '''
-@TODO implement endpoint
+@TODO DONE implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -48,7 +60,16 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-
+@app.route("/drinks", methods=['POST'])
+def post_drinks():
+    body = request.get_json()
+    new_title = body.get('title', None)
+    new_recipe = body.get('recipe', None)
+    drink = Drink(title=title,recipe=recipe)
+    drink.insert()
+    return jsonify({'success': True,
+                    "drinks": drink
+                    })
 
 '''
 @TODO implement endpoint
@@ -61,10 +82,29 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route("/drinks/<id>", methods=['PATCH'])
+def patch_drinks(id):
+    try:
+        drink = Drink.query.filter(Drink.id == id)\
+                                    .one_or_none()
 
+        if drink is None:
+            abort(404)
 
+        body = request.get_json()
+        new_title = body.get('title', None)
+        new_recipe = body.get('recipe', None)
+        drink = Drink(title=title,recipe=recipe)
+        drink.update()
+        datas = Drink.query.all()
+        drinks = [data.long() for data in datas]        
+        return jsonify({'success': True,
+                        "drinks": drink
+                        })
+    except Exception:
+        abort(404)
 '''
-@TODO implement endpoint
+@TODO DONE implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -73,12 +113,23 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route("/drinks/<id>", methods=['DELETE'])
+def delete_drinks():
+    try:
+        drink = Drink.query.filter(Drink.id == id)\
+                                .one_or_none()
 
+        if drink is None:
+            abort(404)
+        
+        drink.delete()
 
+        return jsonify({'success': True,
+                        "delete": id
+                        })
+    except Exception:
+        abort(404)
 ## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -87,24 +138,18 @@ def unprocessable(error):
                     "message": "unprocessable"
                     }), 422
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+        }), 404
 
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": AuthError,
+        "message": "resource not found"
+        }), AuthError
